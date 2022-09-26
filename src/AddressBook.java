88cd1f760ac0915277902fa416bea013affb116f
patri <patrick.ma864@gmail.com>
2022-09-26 16:46:13 -0400

    updated for lab3

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,20 +1,28 @@
 import java.util.ArrayList;
 
 public class AddressBook{
-    ArrayList<BuddyInfo> Book = new ArrayList<>();
+    private ArrayList<BuddyInfo> Book;
 
+    public AddressBook(){
+        Book = new ArrayList<>();
+    }
     public static void main(String[] args) {
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
-        addressBook.removeBuddy(buddy);
+        addressBook.removeBuddy(0);
     }
 
     public void addBuddy(BuddyInfo buddyInfo){
-        Book.add(buddyInfo);
+        if(buddyInfo != null) {
+            Book.add(buddyInfo);
+        }
     }
 
-    public void removeBuddy(BuddyInfo buddyInfo){
-        Book.remove(buddyInfo);
+    public BuddyInfo removeBuddy(int index){
+        if(index >= 0 && index < Book.size()){
+            return Book.remove(index);
+        }
+        return null;
     }
 }
